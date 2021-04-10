906f49d69e5f17bb65c9f7fd9f53ebfa3c94db8b
Alexandra Santiago <santiagoralexandra@gmail.com>
2021-04-10 19:52:47 -0300

    routing in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
 import React from 'react';
+import LandingPage from './components/LandingPage/LandingPage';
+import NavBar from './components/NavBar/NavBar';
+import Recipes from './components/Recipes/Recipes';
+import About from './components/About/About';
+import Favorites from './components/Favorites/Favorites'
+import RecipeDetails from './components/RecipeDetails/RecipeDetails';
 import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-
+      <Route path='/' component={LandingPage} />
+      <Route path='/home' component={NavBar} />
+      <Route exact path='/home' component={Recipes}/>
+      <Route path='/about' component={About} />
+      <Route path='/favorites' component={Favorites}/>
+      <Route path='/recipe/:recipeId' component={RecipeDetails}/>
     </div>
   );
 }
