b4e50c1683d7ffa5df7081880f7c1d302342aa50
Alexandra Santiago <santiagoralexandra@gmail.com>
2021-04-10 21:21:56 -0300

    Landing page done

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,3 +1,18 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import './LandingPage.css';
+import { Link } from 'react-router-dom';
 
+export default function LandingPage (props) {
+  return (
+    <div className='LandingPage'>
+      <div id='info'>
+      <h1 id='AppTitle'>The Food App</h1>
+      <button id='HomeButton'>
+        <Link className='Link' to='/home'>
+        HOME
+        </Link>
+      </button>
+      </div>
+    </div>
+  )
+}
