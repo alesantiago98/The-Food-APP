4d520a3dd7b49a15ef5e97a2a2a94192a56efeb7
Alexandra Santiago <santiagoralexandra@gmail.com>
2021-04-20 13:39:10 -0300

    changes for deploy

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux';
 import store from './redux/Store.js'
 import reportWebVitals from './reportWebVitals';
 import dotenv from "dotenv";
+import axios from 'axios';
 dotenv.config();
 
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
